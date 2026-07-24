5f994751a372e55b1a5e186b17dd753f3ace52b4
Thushara B S <abhibajimar07@gmail.com>
2026-07-24 18:22:57 +0530

    Flash btns

--- script.js
+++ script.js
@@ -1,13 +1,41 @@
 let gameSeq = [];
 let userSeq = [];
+let btns = ["red", "green", "yellow", "blue"];
 
 let level = 0;
 let started = false;
+
+let h2 = document.querySelector("h2");
 
 document.addEventListener("keypress", function() {
     if(started == false){
         console.log("Game Started!");
         started = true;
+
+        levelUp();
     }
-    
 })
+
+function btnFlash(btn){
+    btn.classList.add("flash");
+
+    setTimeout(function() {
+        btn.classList.remove("flash");
+    }, 300);
+}
+
+function levelUp(){
+    level++;
+
+    h2.innerText = "Level " + level;
+
+    let rndIdx = Math.floor(Math.random() * 3);
+    let rndCol = btns[rndIdx];
+    let rndBtn = document.querySelector(`.${rndCol}`);
+
+    console.log(rndIdx);
+    console.log(rndCol);
+    console.log(rndBtn);
+
+    btnFlash(rndBtn);
+}
